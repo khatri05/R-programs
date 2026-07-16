f7d20f1337a27233d8fa2d9a4edfa203a1e3e299
Krishna Khatri <155804075+khatri05@users.noreply.github.com>
2026-07-16 10:01:20 +0530

    New Program added

--- Syllubus Program/Practical10.R
+++ Syllubus Program/Practical10.R
@@ -1,3 +1,4 @@
+library(openxlsx)
 personal <- data.frame(
   Emp_ID = c(101,102,103,104,105),
   Name = c("Krishna","Jash","Bhavya","Rudra","Krisha"),
@@ -12,3 +13,49 @@ write.csv(personal,
 
 print("Personal Details File Created")
 print(personal)
+
+# Save Personal Details to Excel
+write.xlsx(personal,
+           "D:/R Programs/Syllubus Program/personal.xlsx",
+           overwrite = TRUE)
+
+print("Personal Details Excel File Created")
+
+# Create Official Details
+
+official <- data.frame(
+  Emp_ID = c(101,102,103,104,105),
+  Department = c("IT","HR","Finance","IT","Marketing"),
+  Salary = c(30000,35000,45000,32000,50000),
+  Increment = c(12,8,15,5,18)
+)
+
+print(official)
+
+# Save Official Details to Excel
+write.xlsx(official,
+           "D:/R Programs/Syllubus Program/official.xlsx",
+           overwrite = TRUE)
+
+print("Official Details Excel File Created")
+
+personal <- read.xlsx("D:/R Programs/Syllubus Program/personal.xlsx")
+official <- read.xlsx("D:/R Programs/Syllubus Program/official.xlsx")
+
+# Merge Both Files with help of merge()
+employee <- merge(personal,official,by="Emp_ID")
+
+# Calculate Experience
+employee$Experience <- 2026-employee$Joining_Year
+employee$Promotion <- ifelse(
+  employee$Experience >= 5,
+  "Yes",
+  "No"
+)
+
+print(employee)
+write.xlsx(employee,
+           "D:/R Programs/Syllubus Program/official.xlsx",
+           overwrite = TRUE)
+
+print("Promotion Prediction Excel File Created Successfully")
